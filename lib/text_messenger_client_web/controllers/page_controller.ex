f0c08e21e7d53b22acd22d05e361ff29030271eb
Krzysztof Perdek <krzysztofperdek@gmail.com>
2024-10-08 22:18:49 +0200

    Project setup

---
+++ lib/text_messenger_client_web/controllers/page_controller.ex
@@ -0,0 +1,9 @@
+defmodule TextMessengerClientWeb.PageController do
+  use TextMessengerClientWeb, :controller
+
+  def home(conn, _params) do
+    # The home page is often custom made,
+    # so skip the default app layout.
+    render(conn, :home, layout: false)
+  end
+end
