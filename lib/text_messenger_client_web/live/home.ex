997e7b29714896effc7ea151707e5b0cf8d1588d
Krzysztof Perdek <krzysztofperdek@gmail.com>
2024-10-21 18:43:57 +0200

    Contacts list, loading messages based on selected chat

--- lib/text_messenger_client_web/live/home.ex
+++ lib/text_messenger_client_web/live/home.ex
@@ -1,20 +1,35 @@
 defmodule TextMessengerClientWeb.HomePage do
   use TextMessengerClientWeb, :live_view
 
-  def mount(_params, _session, socket) do
-    messages = [
-      {"Uzytkownik2", "Wiadomosc3"},
-      {"Uzytkownik1", "Wiadomosc2"},
-      {"Uzytkownik1", "Wiadomosc1"}
+  @messages %{
+    1 => [
+      {"Uzytkownik2", "Wiadomosc 3 z czatu 1"},
+      {"Uzytkownik1", "Wiadomosc 2 z czatu 1"},
+      {"Uzytkownik1", "Wiadomosc 1 z czatu 1"}
+    ],
+    2 => [
+      {"Uzytkownik2", "Wiadomosc 2 z czatu 2"},
+      {"Uzytkownik1", "Wiadomosc 1 z czatu 2"}
     ]
+  }
 
-    {:ok, assign(socket, messages: messages)}
+  def mount(_params, _session, socket) do
+    contacts = load_contacts()
+    messages = load_messages(1)
+
+    {:ok, assign(socket, messages: messages, contacts: contacts, selected_chat: 1)}
   end
 
   def render(assigns) do
     ~H"""
     <div id="main" class="w-screen h-screen flex bg-black gap-4 p-2">
-      <div id="contacts" class="w-1/3 h-full bg-gray-900 p-2 border-gray-700 rounded-lg"></div>
+      <div id="contacts_box" class="w-1/3 h-full bg-gray-900 p-2 border-gray-700 rounded-lg">
+          <div id="contact_list" class="flex flex-col w-full h-full overflow-y-auto bg-gray-900 p-2 rounded-lg">
+            <%= for {id, name, message} <- @contacts do %>
+              <.live_component module={TextMessengerClientWeb.ContactComponent} id={id} message={message} name={name} selected_chat={@selected_chat} />
+            <% end %>
+        </div>
+      </div>
       <div id="chat" class="flex flex-col grow h-full border-gray-700">
         <div id="chat_messages" class="flex flex-col-reverse w-full h-full overflow-y-auto bg-gray-900 p-2 rounded-lg">
           <%= for {user, message} <- @messages do %>
@@ -40,5 +55,22 @@ defmodule TextMessengerClientWeb.HomePage do
   def handle_event("send_message", %{"message" => message}, socket) do
     socket = assign(socket, messages: [{"debug", message} | socket.assigns.messages])
     {:noreply, socket}
+  end
+
+  def handle_event("select_chat", %{"id" => id}, socket) do
+    chat_id = String.to_integer(id)
+    socket = assign(socket, selected_chat: chat_id, messages: load_messages(chat_id))
+    {:noreply, socket}
+  end
+
+  defp load_contacts() do
+    contacts = [
+      {1, "Czat 1", "Ostatnia wiadomość"},
+      {2, "Czat 2", "Jakaś inna ostatnia wiadomość"}
+    ]
+  end
+
+  defp load_messages(chat_id) do
+    @messages[chat_id]
   end
 end
